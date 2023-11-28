b6e325bdc3b1d0a822d83bcf71229cf280794e1f
Nestor <nestorpuentesin@gmail.com>
2023-11-28 13:34:02 -0600

    se quitaron las seeders de perimeters

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            PerimeterSeeder::class,
+            // PerimeterSeeder::class,
             LevelSeeder::class,
             SchoolSeeder::class,
             DisabilitySeeder::class,
