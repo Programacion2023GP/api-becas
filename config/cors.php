2c5630006edde1bd824abd213da168c4f5b3914a
Programacion2023GP <152647314+Programacion2023GP@users.noreply.github.com>
2023-12-01 15:53:50 -0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*,*'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
